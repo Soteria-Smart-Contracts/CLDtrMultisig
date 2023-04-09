58dd2e32335061e4b992e0b4a8b180dc26c8cc42
Darius Tanz <scratchticketseb@gmail.com>
2023-04-09 15:59:10 +0000

    Apr 9, 2023, 11:59 AM

--- interaction.js
+++ interaction.js
@@ -68,7 +68,6 @@ async function IsSigner(){
     let One = await contract.methods.SignerOne().call();
     let Two = await contract.methods.SignerTwo().call();
     let Three = await contract.methods.SignerThree().call();
-    let Four = await contract.methods.SignerFour().call();
 
     if(account === One || account === Two || account === Three || account === Four){
         return(true);
