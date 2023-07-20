48e28a8a24c117a53fd90023500d96b3b7123aaa
Darius Tanz <scratchticketseb@gmail.com>
2023-07-20 18:27:58 +0000

    Jul 20, 2023, 2:27 PM

---
+++ something.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier:UNLICENSE
+pragma solidity ^0.8.19;
+
+contract VLNDReceiverMinter{
+    address constant Handler = 0x0E3239277501d215e17a4d31c487F86a425E110B;
+
+    struct TX{
+        address Sender;
+        uint256 Payout;
+    }
+    
+    //abi.decode(data, (BridgeRequest))
+    function handle( uint32 _origin, bytes32 _sender, bytes calldata _body ) external onlyMailbox {};
+
+}
+
+interface IMessageRecipient {
+    function handle( uint32 _origin, bytes32 _sender, bytes calldata _body ) external;
+}
