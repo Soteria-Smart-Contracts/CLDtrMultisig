30c278a02170d4f05bac7dfb3063831a9b8ff5ab
Darius Tanz <scratchticketseb@gmail.com>
2023-07-20 18:29:42 +0000

    Jul 20, 2023, 2:29 PM

--- something.sol
+++ something.sol
@@ -19,6 +19,15 @@ contract VLNDReceiverMinter{
         emit ReceivedAndMinted(tx.Sender, tx.Payout, _origin);
     };
 
+    //Pure functions
+
+    function addressToBytes32(address _addr) internal pure returns (bytes32) {
+        return bytes32(uint256(uint160(_addr)));
+    }   
+    function bytes32ToAddress(bytes32 _buf) internal pure returns (address) {
+        return address(uint160(uint256(_buf)));
+    }
+
 }
 
 interface IMessageRecipient {
